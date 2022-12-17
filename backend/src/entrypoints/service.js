bfa2cf412f56f567576307a3fef805f075cff2a5
Tiago Espanha <tiago.espanha@gmail.com>
2022-12-17 15:15:15 -0300

    - Atualiza versões

--- backend/src/entrypoints/service.js
+++ backend/src/entrypoints/service.js
@@ -32,6 +32,12 @@ const buildDatabase = () => {
         host: "localhost",
         port: 5432,
         dialect: 'postgres',
+        pool: {
+            max: 5,
+            min: 0,
+            acquire: 30000,
+            idle: 10000
+        },
     });
 
     return sequelize;
